edaf828de700b77549d509ec2aec0d27c9151ba1
LaoZhang <samuelzhang77@yahoo.com>
2017-10-08 17:02:42 +0800

    turtle: Add some complicated examples
    Add code for sprials

---
+++ part2/draw_circle_spiral.py
@@ -0,0 +1,9 @@
+'''
+draw circle spiral
+'''
+
+import turtle
+t = turtle.Pen()
+for x in range(100):
+    t.circle(x)
+    t.left(91)
---
+++ part2/draw_squair_spiral.py
@@ -0,0 +1,9 @@
+'''
+Draw square spiral
+'''
+
+import turtle
+t = turtle.Pen()
+for x in range(200):
+    t.forward(x)
+    t.left(91)
